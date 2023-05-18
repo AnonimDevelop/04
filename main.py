51c5c964ec7d4103790ce69aeef9e966dd2eb160
Alexandr <a.salauyou@arateg.com>
2023-05-18 21:16:36 +0300

    4.20, 4.25

---
+++ main.py
@@ -0,0 +1,14 @@
+# number = int(input())
+#
+# if number > 10:
+#     print('> 10')
+# else:
+#     print('-')
+
+
+number = int(input())
+
+if number % 2 == 0:
+    print('+')
+else:
+    print('-')
